707a13917160ad9d7863423d8a0781e078ff7ccc
reimarcano <reinaldom88@gmail.com>
2026-07-31 15:36:17 +0200

    Update Friday hours to include evening shift

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -80,7 +80,7 @@ export default function Contacto() {
                      
                       
                       <span className="font-sans text-[13px] text-[#7F7763] min-w-[140px]">Viernes</span>
-                      <span className="font-sans text-[13px] text-[#493523] text-right">8:00 – 14:00</span>
+                      <span className="font-sans text-[13px] text-[#493523] text-right">8:00 – 14:00 | 17:00 - 19:00</span>
                       
                       <span className="font-sans text-[13px] text-[#7F7763] min-w-[140px]">Sáb · Dom</span>
                       <span className="font-sans text-[13px] text-[#493523] text-right">Cerrado</span>
